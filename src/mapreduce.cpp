c52fd80fe236bb88290fa4d6ea7b103a967260dc
rkluzinski <ryankluzinski@gmail.com>
2019-10-27 11:05:12 -0600

    simplify code fixed race condition

--- src/mapreduce.cpp
+++ src/mapreduce.cpp
@@ -1,6 +1,5 @@
 #include <iostream>     // for debugging
 
-
 #include <map>          // for std::multimap
 #include <vector>       // for std::vector
 
@@ -15,208 +14,42 @@ extern "C" {
 #include "threadpool.h"
 }
 
-class Partition {
-    pthread_mutex_t mutex;
-    std::multimap<std::string, std::string> data;
-    std::multimap<std::string, std::string>::iterator it;
-
-public:
-    Partition() {
-        pthread_mutex_init(&mutex, NULL);
-        it = data.begin();
-    }
-
-    ~Partition() {
-        pthread_mutex_destroy(&mutex);
-    }
-
-    void insert(char *key, char *value) {
-        pthread_mutex_lock(&mutex);
-        data.emplace(std::string(key), std::string(value));
-        pthread_mutex_unlock(&mutex);
-    }
-
-    char *next(char *key) {
-        if (it != data.end() && it->first.compare(key) == 0) {
-            return (char *) (it++)->second.c_str();
-        }
-        else {
-            return NULL;
-        }
-    }
-
-    char *nextKey() {
-        if (it != data.end()) {
-            return (char *) it->second.c_str();
-        }
-        else {
-            return NULL;
-        }
-    }
-
-    bool finished() {
-        return it == data.end();
-    }
-};
-
-class MRData {
+struct MRData {
     std::size_t num_partitions;
-    Partition *partitions;
+    
+    pthread_mutex_t *mutex;
+    std::multimap<std::string, std::string> *partition;
+    std::multimap<std::string, std::string>::iterator *partition_it;
 
-public:
     MRData(std::size_t n) {
         num_partitions = n;
-        partitions = new Partition[num_partitions];
+        mutex = new pthread_mutex_t[n];
+        partition = new std::multimap<std::string, std::string>[n];
+        partition_it = new std::multimap<std::string, std::string>::iterator[n];
+
+        for (std::size_t i = 0; i < num_partitions; i++) {
+            pthread_mutex_init(&mutex[i], NULL);
+        }
     }
 
     ~MRData() {
-        delete[] partitions;
-    }
+        for (std::size_t i = 0; i < num_partitions; i++) {
+            pthread_mutex_destroy(&mutex[i]);
+        }
 
-    std::size_t size() {
-        return num_partitions;
-    }
-
-    void insert(std::size_t n, char *key, char *value) {
-        partitions[n].insert(key, value);
-    }
-
-    char *next(std::size_t n, char *key) {
-        return partitions[n].next(key);
-    }
-
-    char *nextKey(std::size_t n) {
-        return partitions[n].nextKey();
-    }
-
-    bool finished(std::size_t n) {
-        return partitions[n].finished();
+        delete[] mutex;
+        delete[] partition;
+        delete[] partition_it;
     }
 };
 
 // MapReduce shared data
 // declared static so it cannot be accessed from other files
-static MRData *shared_data;
+MRData *shared_data;
 
 // Not passed to MR_ProcessPartition
 // so stored as a global variable instead
-static Reducer g_reducer;
-
-// class MapReduce {
-// private:
-//     // shared data structure
-//     std::vector<pthread_mutex_t> locks;
-//     std::vector<std::multimap<std::string, std::string>> partitions;
-
-//     // iterators used by getNext
-//     std::vector<std::multimap<std::string, std::string>::iterator> iterators; 
-
-// public:
-//     MapReduce(int n_mappers, int n_reducers);
-//     ~MapReduce();
-
-//     // MapReduce functions
-//     void map(Mapper mapper, int num_files, char *filenames[]);
-//     void reduce(Reducer reducer);
-
-//     // called by Mapper and Reducer threads
-//     void emit(char* key, char* value);
-//     void processPartition(int partition_number);
-//     char *getNext(char* key, int partition_num);
-// };
-
-// // global MapReduce object
-// static MapReduce *mapreduce;
-
-// /**
-//  * MapReduce Constructor
-//  * Parameters:
-//  *      n_mappers - The number of mapper threads to create
-//  *      n_reducers - The number of reducer threads to create
-//  */
-// MapReduce::MapReduce(int n_mappers, int n_reducers) {
-    
-//     // create mutexes and multimaps
-//     for (int i = 0; i < num_reducers; i++) {
-//         locks.emplace_back();
-//         pthread_mutex_init(&locks.back(), NULL);
-
-//         partitions.emplace_back();
-//         iterators.emplace_back();
-//     }
-// }
-
-// /**
-//  * MapReduce Destructor
-//  */
-// MapReduce::~MapReduce() {
-//     // destory mutexes
-//     for (auto &mutex : locks) {
-//         pthread_mutex_destroy(&mutex);
-//     }
-// }
-
-// void Reducer_entry(int *arg) {
-//     MR_ProcessPartition(*arg);
-// }
-
-// /**
-//  * Create and run reducer threads
-//  * Parameters:
-//  *      reducer - The reducer function to apply
-//  */
-// void MapReduce::reduce(Reducer reducer) {
-//     reduce_fn = reducer;
-//     ThreadPool_t *reducerPool = ThreadPool_create(num_reducers);
-
-//     // arguments MR_ProcessPartition 
-//     int *args = new int[num_reducers];
-//     for (int i = 0; i < num_reducers; i++) {
-//         // allows integer to be passed to new thread
-//         args[i] = i;
-//         ThreadPool_add_work(reducerPool, 
-//             (thread_func_t) Reducer_entry, (void *) &args[i]);
-//     }
-//     delete args;
-
-//     ThreadPool_destroy(reducerPool);
-// }
-
-// /**
-//  * Emit a key-value pair to the shared data structure
-//  * Parameters:
-//  *      key - The key to emit
-//  *      value - The value to emit
-//  */
-// void MapReduce::emit(char* key, char* value) {
-//     unsigned long partition = MR_Partition(key, num_reducers);
-//     pthread_mutex_lock(&locks.at(partition));
-//     partitions[partition].emplace(std::string(key), std::string(value));
-//     pthread_mutex_unlock(&locks.at(partition));
-// }
-
-// /**
-//  * 
-//  */
-// void MapReduce::processPartition(int partition_number) {
-//     pthread_mutex_lock(&locks.at(partition_number));
-    
-//     auto partition = partitions.at(partition_number);
-//     auto i = partition.begin();
-//     while (i != partition.end()) {
-//         iterators.assign(partition_number, i);
-//         reduce_fn((char *) (i++)->first.c_str(), partition_number);
-//     }
-
-//     pthread_mutex_unlock(&locks.at(partition_number));
-// }
-
-// /**
-//  * 
-//  */
-// char *MapReduce::getNext(char *key, int partition_number) {
-//     return NULL;
-// }
+Reducer g_reducer;
 
 /**
  * 
@@ -256,16 +89,16 @@ void MR_Map(int num_files, char *filenames[], Mapper map, int num_mappers) {
 void MR_Reduce(Reducer reducer, int num_reducers) {
     g_reducer = reducer;
     
-    ThreadPool_t *reducerPool = ThreadPool_create(num_reducers);
     int *args = new int[num_reducers];
+    ThreadPool_t *reducerPool = ThreadPool_create(num_reducers);
     
     for (int i = 0; i < num_reducers; i++) {
         args[i] = i;
         ThreadPool_add_work(reducerPool, (thread_func_t) Reducer_Entry, &args[i]);
     }
     
-    delete[] args;
     ThreadPool_destroy(reducerPool);
+    delete[] args;
 }
 
 /**
@@ -296,8 +129,10 @@ void MR_Run(int num_files, char *filenames[],
  *      value - The value to associate to that key
  */
 void MR_Emit(char *key, char *value) {
-    std::size_t partition = MR_Partition(key, shared_data->size());
-    shared_data->insert(partition, key, value);
+    std::size_t index = MR_Partition(key, shared_data->num_partitions);
+    pthread_mutex_lock(&shared_data->mutex[index]);
+    shared_data->partition[index].emplace(key, value);
+    pthread_mutex_unlock(&shared_data->mutex[index]);
 }
 
 /**
@@ -322,8 +157,12 @@ unsigned long MR_Partition(char *key, int num_partitions) {
  *      partition_number - The partition to process
  */
 void MR_ProcessPartition(int partition_number) {
-    while(!shared_data->finished(partition_number)) {
-        char *key = shared_data->nextKey(partition_number);
+    auto &partition = shared_data->partition[partition_number];
+    /* const */ auto &it = shared_data->partition_it[partition_number];
+    
+    it = partition.begin();
+    while(it != partition.end()) {
+        char *key = (char *) it->first.c_str();
         g_reducer(key, partition_number);
     }
 }
@@ -335,5 +174,13 @@ void MR_ProcessPartition(int partition_number) {
  *      partition_number - The partition number to look in
  */
 char *MR_GetNext(char *key, int partition_number) {
-    return shared_data->next(partition_number, key);
+    /* const */ auto end = shared_data->partition[partition_number].end();
+    /* const */ auto &it = shared_data->partition_it[partition_number];
+
+    if (it != end && it->first.compare(key) == 0) {
+        return (char *) (it++)->second.c_str();
+    }
+    else {
+        return NULL;
+    }
 }
